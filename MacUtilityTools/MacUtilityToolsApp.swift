d1f0766a9ca52520cbb4928c1e3c2c53d9026110
Thibaut Monin <device.thibaut.monin@gmail.com>
2024-04-28 14:44:31 +0200

    add command + q shortcut to quit app

--- MacUtilityTools/MacUtilityToolsApp.swift
+++ MacUtilityTools/MacUtilityToolsApp.swift
@@ -6,14 +6,26 @@
 //
 
 import SwiftUI
+import AppKit
 
 @main
 struct MacUtilityToolsApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     @StateObject var toolState = ToolState()
     
     var body: some Scene {
         MenuBarExtra("MacUtilityTools", image: "MacUtilityToolsIcon") {
             ToolsView(toolState: toolState)
         }.menuBarExtraStyle(.window)
+    }
+}
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        NSEvent.addGlobalMonitorForEvents(matching: .keyDown) { event in
+            if event.modifierFlags.contains(.command) && event.keyCode == 12 {
+                NSApp.terminate(self)
+            }
+        }
     }
 }
